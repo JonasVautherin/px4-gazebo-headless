4b414948d948f80b7b3ecc9eb262039c5eed6c98
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-03-14 00:26:18 +0100

    add mavlink interface IPs as parameters

--- px4-gazebo-headless/README.md
+++ px4-gazebo-headless/README.md
@@ -1,15 +1,31 @@
 In order to run a PX4-Gazebo simulator (headless):
 
-1. Build the image from this git repository:
+## Build the image from this git repository:
 
 ```
 $ docker build https://github.com/JonasVautherin/docker.git#master:px4-gazebo-headless -t px4-gazebo-headless
 ```
 
-2. Run it:
+## Run it in BROADCAST mode:
 
 ```
 $ docker run --rm -it px4-gazebo-headless
 ```
 
 `MAV_BROADCAST` is enabled by default, and the second MAVLink interface is not run in "onboard" mode in order to enable the broadcasting. Those changes are made by [edit_iris_config.bash](edit_iris_config.bash).
+
+## Run it with a custom IP for the second mavlink interface
+
+```
+$ docker run --rm -it px4-gazebo-headless 192.168.0.12
+```
+
+where `192.168.0.12` should be replaced by the IP listening on the API port 14540 (e.g. DroneCore or Mavros).
+
+## Run it with custom IP for both mavlink interfaces
+
+```
+$ docker run --rm -it px4-gazebo-headless 192.168.0.10 10.0.0.12
+```
+
+where `192.168.0.10` should be replaced by the IP listening on the QGC port (e.g. QGroundControl) and `10.0.0.12` should be replaced by the IP listening on the API port (e.g. DroneCore or Mavros).
--- px4-gazebo-headless/edit_iris_config.bash
+++ px4-gazebo-headless/edit_iris_config.bash
@@ -1,7 +1,32 @@
 #!/bin/bash
 
+function is_ip_valid {
+    if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        return 0
+    else
+        echo "Invalid IP: $1"
+        return 1
+    fi
+}
+
+if [ "$#" -eq 1 ]; then
+    if ! is_ip_valid $1; then exit 1; fi
+    echo "14540 will be associated to $1"
+    API_PARAM="-t $1"
+elif [ "$#" -eq 2 ]; then
+    if ! is_ip_valid $1 || ! is_ip_valid $2; then exit 1; fi
+    echo "14550 will be associated to $1"
+    echo "14540 will be associated to $2"
+    QGC_PARAM="-t $1"
+    API_PARAM="-t $2"
+elif [ "$#" -gt 2 ]; then
+    echo "Invalid parameters: [<IP for 14550> <IP for 14540>] | [<IP for 14540>]"
+    exit 1;
+fi
+
 IRIS_CONFIG_FILE=${FIRMWARE_DIR}/posix-configs/SITL/init/ekf2/iris
 
-sed -i "s/mavlink start \-x \-u 14557 -r 4000000 -m onboard -o 14540/mavlink start \-x -u 14557 -r 4000000 -o 14540 /" ${IRIS_CONFIG_FILE}
+sed -i "s/mavlink start \-x \-u 14556 -r 4000000/mavlink start \-x -u 14556 -r 4000000 ${QGC_PARAM}/" ${IRIS_CONFIG_FILE}
+sed -i "s/mavlink start \-x \-u 14557 -r 4000000 -m onboard -o 14540/mavlink start \-x -u 14557 -r 4000000 -o 14540 ${API_PARAM}/" ${IRIS_CONFIG_FILE}
 
 echo 'param set MAV_BROADCAST 1' >> ${IRIS_CONFIG_FILE}
