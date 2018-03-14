9499bfc3c3ec0dacd6a0a02a19a0624d347c37e1
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-03-14 14:53:53 +0100

    add support for mac host

--- px4-gazebo-headless/edit_iris_config.bash
+++ px4-gazebo-headless/edit_iris_config.bash
@@ -9,6 +9,20 @@ function is_ip_valid {
     fi
 }
 
+function is_docker_for_mac {
+    getent hosts docker.for.mac.host.internal >/dev/null 2>&1
+    return $?
+}
+
+function get_mac_host_ip {
+    if ! is_docker_for_mac; then
+        echo "ERROR: this is not a docker for mac container!"
+        exit 1
+    fi
+
+    echo "$(getent hosts docker.for.mac.host.internal | awk '{ print $1 }')"
+}
+
 if [ "$#" -eq 1 ]; then
     if ! is_ip_valid $1; then exit 1; fi
     echo "14540 will be associated to $1"
@@ -24,9 +38,16 @@ elif [ "$#" -gt 2 ]; then
     exit 1;
 fi
 
+# Broadcast doesn't work with docker for mac, so we default to the mac host (docker.for.mac.localhost)
+if is_docker_for_mac; then
+    MAC_HOST=$(get_mac_host_ip)
+    QGC_PARAM=${QGC_PARAM:-"-t ${MAC_HOST}"}
+    API_PARAM=${API_PARAM:-"-t ${MAC_HOST}"}
+fi
+
 IRIS_CONFIG_FILE=${FIRMWARE_DIR}/posix-configs/SITL/init/ekf2/iris
 
-sed -i "s/mavlink start \-x \-u 14556 -r 4000000/mavlink start \-x -u 14556 -r 4000000 ${QGC_PARAM}/" ${IRIS_CONFIG_FILE}
-sed -i "s/mavlink start \-x \-u 14557 -r 4000000 -m onboard -o 14540/mavlink start \-x -u 14557 -r 4000000 -o 14540 ${API_PARAM}/" ${IRIS_CONFIG_FILE}
+sed -i "s/mavlink start \-x \-u 14556 -r 4000000$/mavlink start \-x -u 14556 -r 4000000 ${QGC_PARAM}/" ${IRIS_CONFIG_FILE}
+sed -i "s/mavlink start \-x \-u 14557 -r 4000000 -m onboard -o 14540$/mavlink start \-x -u 14557 -r 4000000 -o 14540 ${API_PARAM}/" ${IRIS_CONFIG_FILE}
 
 echo 'param set MAV_BROADCAST 1' >> ${IRIS_CONFIG_FILE}
